422d1b60c265d4d044f014118a0f7cfe88f2e253
Martin Macháček <martin.mach.machacek@gmail.com>
2022-01-01 11:13:13 +0100

    Add cookie consent for GDPR, fill SEO head attributes

--- README.md
+++ README.md
@@ -8,6 +8,18 @@ Starting project for development purposes:
 
 `yarn start` - clear cache and start development server
 
+## Deployment
+
+Website is hosted on Netlify.
+
+- [Admin panel for production web](https://app.netlify.com/sites/vork-prod/overview)
+- [Admin panel for development](https://app.netlify.com/sites/vork-dev/overview)
+
+Builds and deploys are automated, with setting:
+
+- Develop branch is deployed here: https://vork-dev.netlify.app
+- Main branch is deployed here: https://www.vork.cz
+
 ## TODOs
 
 Redefine data model
--- src/templates/Page.js
+++ src/templates/Page.js
@@ -3,12 +3,39 @@ import { arrayOf, shape, string } from 'prop-types';
 
 import Layout from '../components/Layout';
 import SectionRenderer from './SectionRenderer';
+import { Helmet } from 'react-helmet';
+import SEO from '../components/seo';
 
-const Template = ({ pageContext }) => (
+const Template = ({ pageContext, ...other }) => (
 	<Layout>
+		<Helmet>
+			{/* Cookie consent for GDPR */}
+			<script
+				id="Cookiebot"
+				src="https://consent.cookiebot.com/uc.js"
+				data-cbid="1cfb8790-d0cf-4c76-b9e4-b8bddcf13d05"
+				data-blockingmode="auto"
+				type="text/javascript"
+			></script>
+		</Helmet>
+
+		<SEO
+			description={pageContext.seoDescription}
+			title={pageContext.seoTitle}
+			meta={pageContext.defaultSeo}
+		/>
+
+		{console.log({ pageContext, other })}
 		{pageContext.sections.map((sectionData) => (
 			<SectionRenderer key={sectionData.sectionTitle} {...sectionData} />
 		))}
+
+		<script
+			id="CookieDeclaration"
+			src="https://consent.cookiebot.com/1cfb8790-d0cf-4c76-b9e4-b8bddcf13d05/cd.js"
+			type="text/javascript"
+			async
+		></script>
 	</Layout>
 );
 
